5959d6f7ac043abd33bf3141df5c93d07ed0ecfc
Luiz727 <luizreis.dev@gmail.com>
2025-12-15 16:24:02 -0300

    feat: rebrand to Nixcon Browser - remove GitHub link, update title and logo text

--- client/src/components/connect.vue
+++ client/src/components/connect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="connect">
     <div class="window">
-      <div class="logo" title="About n.eko" @click.stop.prevent="about">
-        <img src="@/assets/images/logo.svg" alt="n.eko" />
-        <span><b>n</b>.eko</span>
+      <div class="logo" title="Nixcon Browser" @click.stop.prevent="about">
+        <img src="@/assets/images/logo.svg" alt="Nixcon Browser" />
+        <span>Nixcon <b>Browser</b></span>
       </div>
       <form class="message" v-if="!connecting" @submit.stop.prevent="connect">
         <span v-if="!autoPassword">{{ $t('connect.login_title') }}</span>
